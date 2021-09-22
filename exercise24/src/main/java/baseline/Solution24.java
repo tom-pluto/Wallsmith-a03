2b75e2f186a8fbc09b9d06306c5f3da93ee36d11
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-09-21 22:38:45 -0400

    exercise24 setup

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Thomas Wallsmith
+ *
+ *  Exercise 24 - Anagram Checker
+ *
+ *  - Enter two strings and I'll tell you if they are anagrams:
+ *  - Enter the first string: note
+ *  - Enter the second string: tone
+ *  - "note" and "tone" are anagrams.
+ *
+ *  Constraints:
+ *  - Implement the program using a function called <isAnagram> which takes in two
+ *    words as its arguments and returns true or false
+ */
+
+package baseline;
+
+public class Solution24 {
+    public static void main(String[] args) {
+
+        // Prompt for & read in string a
+        // Prompt for & read in string b
+
+        // If isAnagram(a, b) == True
+            // Print a and b are anagrams
+        // Else
+            // Print a and b are not anagrams
+
+
+    }
+    private static boolean isAnagram(String a, String b){
+
+        // If the length of the two strings are not the same
+            // Return False
+
+        // Sort a alphabetically
+        // Sort b alphabetically
+
+        // If a equals b
+            // Return True
+        // Else
+            // Return False
+    }
+}
