2208f184abf4f8ad4f1e175351952572e982a02d
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-09-24 21:09:49 -0400

    exercise29 setup

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Thomas Wallsmith
+ */
+
+package baseline;
+
+public class Solution29 {
+    public static void main(String[] args) {
+
+        //Call getRate() to prompt and read in valid input
+        //Call getYears(input) to get number of years needed to double the investment
+        //Print output
+
+    }
+
+    public static String getYears(double rate) {
+
+        /*
+        Calculates the number of years needed to double the initial investment.
+        Returns string stating the number of years needed to double the initial investment.
+        Formula : 72 / r
+         */
+
+        //years = (72 / rate)
+        //Return "It will take <rate> years to double your initial investment"
+
+        return "";
+    }
+
+    private static double getRate() {
+
+        /*
+        Prompts for the rate of return and only accepts a numeric value
+         */
+
+        //Loop
+            //Prompt "What is the rate of return? " & read for rate
+            //If rate is a number and greater than 0, break the loop
+            //Else, print "Sorry. That's not a valid input.\n"
+        //Return rate
+
+        return 0;
+    }
+}
---
+++ exercise29/src/test/java/baseline/Solution29Test.java
@@ -0,0 +1,15 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution29Test {
+
+    @Test
+    void getYears() {
+        String expected = "It will take 18 years to double your initial investment.";
+        String actual = Solution29.getYears(72.0/4);
+        assertEquals(expected, actual);
+    }
+}
