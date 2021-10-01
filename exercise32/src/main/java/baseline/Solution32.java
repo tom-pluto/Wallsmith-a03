a9fdaa70b52b1228159373e76baf6e4e9318cdd2
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-10-01 01:29:18 -0400

    exercise32 completed

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -19,66 +19,200 @@ Exercise 32 - Guess the Number Game
 
 package baseline;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution32 {
+
+    private static final Scanner sc = new Scanner(System.in);
+    private static final String PLEASE_ENTER_A_VALID_INPUT = "Please enter a valid input.";     //SonarLint made me do this.
+    private static final Random rand = new Random();
 
     public static void main(String[] args) {
 
-        //Display welcome message
-        //Call startRandomGame
+        Solution32 solution = new Solution32();
+
+        //Print out Welcome Message
+        System.out.printf("%s%n", "Let's play Guess the Number!");
+
+        //Call startRandomGame() to start the main game loop
+        solution.startRandomGame();
 
     }
 
-    private static void startRandomGame() {
+    private void startRandomGame() {
 
         /*
         Handles multiple replaying of the random game. Prompts for difficulty before generating answer and starting.
         At the end, the game will ask the user if they would like to play again. If yes, the method loops.
          */
 
-        //Loop
-            //Prompt for difficulty
-            //If difficulty == 1
-                //max = 10
-            //If difficulty == 2
-                //max = 100
-            //If difficulty == 3
-                //max = 1000
-            //answer = generateRandomInt(1, max)
-            //Prompt for user to play again
-                //If yes    : do nothing and loop again
-                //If no     : break loop
+        while(true) {
+            playRandomGame(generateRandomInt(getMax()));
+            String answer = getYN();
+            if(answer.equals("N")) return;
+        }
     }
 
-    private static void playRandomGame(int answer) {
+    private String getYN() {
+
+        /*
+        Prompts the user if they want to play again and ONLY accepts a Y or N response (non-case sensitive)
+         */
+
+        while(true) {
+            System.out.printf("%s ", "Do you wish to play again (Y/N)?");       //Prompt user
+            String answer = sc.next();                                          //Read input
+            answer = answer.toUpperCase();                                      //Make capital
+
+            if(answer.equals("N") || answer.equals("Y"))                        //If input is "Y" or "N"
+                return answer;                                                      //Return input
+            else                                                                //Otherwise
+                System.out.printf("%s%n", PLEASE_ENTER_A_VALID_INPUT);           //Prompt user to enter valid input
+        }
+    }
+
+    private int getMax() {
+
+        /*
+        Gets the difficulty from the user (using getDifficult()) and then converts it to the maximum random
+        number (using detemineMax())
+         */
+        return determineMax(getDifficulty());
+    }
+
+    private int getDifficulty() {
+        /*
+        Prompts the user for the difficulty level, and only accepts the input if it's 1, 2, or 3
+         */
+        int difficulty;
+        while(true) {
+            System.out.printf("%n%s ", "Enter the difficulty level (1, 2, or 3):");
+            String input = sc.next();
+
+            String enterValid = PLEASE_ENTER_A_VALID_INPUT;
+            try {
+                difficulty = Integer.parseInt(input);   //will throw NumberFormatException to be caught if invalid
+                if(difficulty == 1 || difficulty == 2 || difficulty == 3) {
+                    break;
+                    //Is valid!
+                }
+                else {
+                    System.out.printf("%s%n", enterValid);
+                    //Tries again
+                }
+            } catch (NumberFormatException e) {
+                System.out.printf("%s%n", enterValid);
+                //Tries again
+            }
+        }
+        return difficulty;
+    }
+
+    public boolean is123(String input) {
+
+        /*
+        Determines if the passed string is the integers 1, 2, or 3
+         */
+
+        //Tests if the input is an integer
+        if(isInteger(input)) {
+
+            //Convert to integer
+            int num = Integer.parseInt(input);
+
+            //Test if number is 1 2 or 3
+            return num == 1 || num == 2 || num == 3;
+        }
+        else //not a integer
+            return false;
+    }
+
+    public int determineMax(int difficulty) {
+
+        /*
+        Returns the corresponding max random number for requested difficulty.
+        1 : 10
+        2 : 100
+        3 : 1000
+         */
+
+        if(difficulty == 1) {
+            return 10;
+        }
+        else if(difficulty == 2) {
+            return 100;
+        }
+        else {
+            return 1000;
+        }
+    }
+
+    private void playRandomGame(int answer) {
 
         /*
         Runs the random game with the answer being passed to it. Different from startGame() since this simply runs the
         game and does not generate an answer or asks to play again.
          */
 
-        //Prompt user for their initial guess
-        //Loop
-            //Read in userGuess
-            //If userGuess > answer
-                //Print that guess is too large and to guess again
-                //Increment n
-            //If userGuess < answer
-                //Print that the guess is too large and to guess again
-                //Increment n
-            //If userGuess == answer
-                //Print that they got the guess right in n guesses
-                //Break loop
+        int tries = 0;
+        int guess = getIntegerInput("I have my number. What's your guess? ");
+
+        while(true) {
+            tries++;
+
+            //I originally had an if else tree here, but SonarLint didn't like it. Not sure why...
+            if(guess > answer) {   //If guess is too high
+                guess = getIntegerInput("Too high. Guess again: ");
+            }
+            if(guess < answer) {   //If guess is too low
+                guess = getIntegerInput("Too low. Guess again: ");
+            }
+            if(guess == answer) {  //If guess is correct
+                System.out.printf("You got it in %d guesses!%n%n", tries);
+                break;
+            }
+        }
     }
 
-    private static int generateRandomInt(int min, int max) {
+    private int getIntegerInput(String prompt) {
+
+        /*
+        Prints a prompt and reads in an input from the console. Only returns the input if it is an integer.
+         */
+
+        while(true) {
+            System.out.printf("%s", prompt);        //Prompts user for input
+            String input = sc.next();               //Read input
+            try {                                   //Tries to convert to integer
+                return Integer.parseInt(input);         //Returns if valid
+            } catch(NumberFormatException e) {
+                System.out.printf("%s%n", PLEASE_ENTER_A_VALID_INPUT);
+            }
+        }
+    }
+
+    public boolean isInteger(String input) {
+
+        /*
+        Determines if a passed string input is a valid integer. If yes, returns true. If not, returns false.
+         */
+
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+
+    }
+
+    private int generateRandomInt(int max) {
 
         /*
         Generates random number between integers min and max.
          */
 
-        //Range = (max - min) + 1
-        //Return (int)(Math.random() * Range) + min
-
-        return 0;
+        return rand.nextInt(max);
     }
 }
--- exercise32/src/test/java/baseline/Solution32Test.java
+++ exercise32/src/test/java/baseline/Solution32Test.java
@@ -1,15 +1,63 @@
 package baseline;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution32Test {
-    /*
 
-    TO GRADER:  I didn't include any JUnit5 tests for this exercise because I really don't know how to implement them.
-                All of the data in this class is pretty much either related to a random number or console input,
-                and we weren't shown how to simulate console inputs, so... please no points off. I'll try to include them
-                later if it makes sense.
+    Solution32 test = new Solution32();
 
-    */
+    @Test
+    @DisplayName("is123()")
+    void is123() {
 
+        boolean expected = true;
+        boolean actual1 = test.is123("1");
+        boolean actual2 = test.is123("2");
+        boolean actual3 = test.is123("3");
+
+        boolean expected2 = false;
+        boolean actualf1 = test.is123("4");
+        boolean actualf2 = test.is123("5");
+        boolean actualf3 = test.is123("6");
+
+        assertEquals(expected, actual1, "Testing 1");
+        assertEquals(expected, actual2, "Testing 2");
+        assertEquals(expected, actual3, "Testing 3");
+        assertEquals(expected2, actualf1, "Testing 4");
+        assertEquals(expected2, actualf2, "Testing 5");
+        assertEquals(expected2, actualf3, "Testing 6");
+    }
+
+    @Test
+    @DisplayName("detemineMax()")
+    void determineMax() {
+        int t1 = 1;
+        int t2 = 2;
+        int t3 = 3;
+        int expected1 = 10;
+        int expected2 = 100;
+        int expected3 = 1000;
+        int actual1 = test.determineMax(t1);
+        int actual2 = test.determineMax(t2);
+        int actual3 = test.determineMax(t3);
+        assertEquals(expected1, actual1, "Case 1");
+        assertEquals(expected2, actual2, "Case 2");
+        assertEquals(expected3, actual3, "Case 3");
+    }
+
+    @Test
+    @DisplayName("isInteger()")
+    void isInteger() {
+        String t1 = "100";
+        String t2 = "not an integer";
+        boolean expected1 = true;
+        boolean expected2 = false;
+        boolean actual1 = test.isInteger(t1);
+        boolean actual2 = test.isInteger(t2);
+        assertEquals(expected1, actual1, "True case");
+        assertEquals(expected2, actual2, "False case");
+    }
 }
