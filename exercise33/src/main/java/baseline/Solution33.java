3abe2f0ea93e1eae418f071b6fc56463a768f758
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-10-01 02:04:27 -0400

    exercise33 setup

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,63 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Thomas Wallsmith
+ */
+
+package baseline;
+
+public class Solution33 {
+    
+    //Shared Scanner declaration goes here
+    //Shared Random declaration goes here
+
+    //Main method
+    public static void main(String[] args) {
+        
+        //Ask for user's question
+        //Print random response
+        
+    }
+
+    //Functions that I can feasably test
+    public String getRandomResponse() {
+        
+        /*
+        Gets a random number 1-4 and uses it to choose between 4 responses:
+                "Yes."       "No."        "Maybe."     "Ask again later."
+         */
+        
+        //Get Random number w/ getRandomNumber call
+        //If the random number equals 1
+            //Return "Yes."
+        //If the random number equals 2
+            //Return "No."
+        //If the random number equals 3
+            //Return "Maybe."
+        //If the random number equals 4
+            //Return "Ask again later."
+        
+        return "";
+    }
+    public int getRandomNumberBetween1And4() {
+        
+        /*
+        Creates a random number inbetween 1 and 4 inclusive
+         */
+        
+        //Returns random number between 1 and 4 inclusive
+        
+        return 0;
+    }
+
+    //Functions that I cannot feasably test
+    private void askQuestion() {
+        
+        /*
+        Asks for a user's question, and then completes disregards it because WHO CARES LOL!
+         */
+        
+        //Prompt user for question
+        //Read in question into nothing because nobody cares what you have to say they all just pretend to care anyways
+        
+    }
+}
---
+++ exercise33/src/test/java/baseline/Solution33Test.java
@@ -0,0 +1,36 @@
+package baseline;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Solution33")
+class Solution33Test {
+
+    Solution33 test = new Solution33();
+
+    @Test
+    @DisplayName("getRandomResponse()")
+    void getRandomResponse() {
+
+        String actual = test.getRandomResponse();
+        assertTrue(actual == "Yes."
+                || actual == "No."
+                || actual == "Maybe."
+                || actual == "Ask again later.",
+                "Testing if any valid response is given");
+
+    }
+
+    @Test
+    @DisplayName("getRandomNumberBetween1And4()")
+    void getRandomNumberMin1Max4() {
+
+        //Epic loop to test randomness (which you cant but...)
+        for (int i = 0; i < 10; i++) {
+            int actual = test.getRandomNumberBetween1And4();
+            assertTrue(actual >= 1 && actual <= 4, "Is it between 1 and 4?");
+        }
+    }
+}
