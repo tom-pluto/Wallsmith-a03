2c298afdccce59c999ed6c576fdd6774a5627b19
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-10-03 19:41:26 -0400

    exercise37 setup

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,46 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Thomas Wallsmith
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution37 {
+
+    //Constant list of numbers goes here
+    //Constant list of letters goes here
+    //Constant list of special characters goes here
+
+    public static void main(String[] args) {
+
+        //Get inputs from user                  getIntegerInput()
+        //Create password                       generatePassword()
+        //Print password
+
+    }
+
+    public String generatePassword(int minLength, int numSpecialChar, int numNumbers) {
+
+        //Add number of letters to the string
+        //Add number of special characters to the string
+        //Add number of numbers to the string
+        //Shuffle the string
+        //Return the string
+
+        return "";
+    }
+
+    private int getIntegerInput() {
+
+        //Loop
+            //Prompt user
+            //Read input
+            //Attempt to convert input to int
+                //Works : Return converted input
+                //Fails : Print error Loop again
+
+        return 0;
+    }
+}
