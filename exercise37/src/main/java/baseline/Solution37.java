0505d0f5e7e0433661a397333dcb6ca1f5a72294
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-10-03 20:51:37 -0400

    exercise37 completed

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -5,42 +5,93 @@
 
 package baseline;
 
-import java.util.List;
+import java.util.*;
 
 public class Solution37 {
 
-    //Constant list of numbers goes here
-    //Constant list of letters goes here
-    //Constant list of special characters goes here
+    private final Scanner sc = new Scanner(System.in);
+    private final Random rand = new Random();
 
     public static void main(String[] args) {
 
-        //Get inputs from user                  getIntegerInput()
-        //Create password                       generatePassword()
-        //Print password
+        Solution37 sol = new Solution37();
 
+        //Get inputs from user
+        int minLength = sol.getIntegerInput("What's the minimum length?");
+        int numSpecialChar = sol.getIntegerInput("How many special characters?");
+        int numNumbers = sol.getIntegerInput("How many numbers?");
+
+        //Create password & print
+        System.out.printf("Your password is %s%n", sol.generatePassword(minLength, numSpecialChar, numNumbers));
     }
 
     public String generatePassword(int minLength, int numSpecialChar, int numNumbers) {
 
-        //Add number of letters to the string
-        //Add number of special characters to the string
-        //Add number of numbers to the string
-        //Shuffle the string
-        //Return the string
+        final List<Character> SPECIALCHARS = Arrays.asList('!', '@', '#', '$', '%', '^', '&', '*' , '_', '+');
+        final List<Character> LETTERS = Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+        final List<Character> NUMBERS = Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
 
-        return "";
+        List<Character> output = new ArrayList<>();
+
+        //Add number of numbers to the string
+        for (int i = 0; i < numNumbers; i++) {
+
+            output.add(NUMBERS.get(rand.nextInt(10)));
+
+        }
+
+        //Add number of special characters to the string
+        for (int i = 0; i < numSpecialChar; i++) {
+
+            output.add(SPECIALCHARS.get(rand.nextInt(10)));
+
+        }
+
+        //Add number of letters to the string
+        //(making sure there are at least as many letters as there are special characters and numbers)
+        for (int i = 0; i < numSpecialChar+numNumbers; i++) {
+
+            output.add(LETTERS.get(rand.nextInt(26)));
+
+        }
+
+        //Shuffle the string
+        Collections.shuffle(output);
+
+        //Create regular looking string
+        StringBuilder sOutput = new StringBuilder();
+        for (int i = 0; i < output.size(); i++) {
+            sOutput.append(output.get(i));
+        }
+
+        //Return the string
+        return sOutput.toString();
+
     }
 
-    private int getIntegerInput() {
+    private int getIntegerInput(String prompt) {
 
         //Loop
-            //Prompt user
-            //Read input
-            //Attempt to convert input to int
-                //Works : Return converted input
-                //Fails : Print error Loop again
+        while(true) {
 
-        return 0;
+            //Prompt user
+            System.out.printf("%s ", prompt);
+
+            //Read input
+            String input = sc.nextLine();
+
+            //Attempt to convert input to int
+            try {
+
+                //Works : Return converted input
+                return Integer.parseInt(input);
+
+            } catch(NumberFormatException e) {
+
+                //Fails : Print error Loop again
+                System.out.printf("%s%n", "Please enter an integer input.");
+
+            }
+        }
     }
 }
---
+++ exercise37/src/test/java/baseline/Solution37Test.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Solution37")
+class Solution37Test {
+
+    private Solution37 test = new Solution37();
+
+    @Test
+    @DisplayName("generatePassword()")
+    void generatePassword() {
+
+        String actual1 = test.generatePassword(8,2,2);
+        String actual2 = test.generatePassword(8,2,2);
+        System.out.printf("actual 1 = %s%n" +
+                "actual 2 = %s", actual1, actual2);
+        assertNotEquals(actual1, actual2, "Do two random generated strings match?");
+    }
+}
